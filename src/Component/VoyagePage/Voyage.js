8bcecf0c03550424c57e41538ee4f025e0fd53df
scymz1 <mz50@rice.edu>
2022-06-10 14:31:37 -0500

    remove token

--- src/Component/VoyagePage/Voyage.js
+++ src/Component/VoyagePage/Voyage.js
@@ -3,6 +3,11 @@ import Result from "./Result/Result";
 import {useState} from "react";
 import React from "react";
 import {Container} from "@mui/material";
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export default function Voyage() {
     return (
@@ -10,6 +15,7 @@ export default function Voyage() {
         <Container sx={{border: 1}}>
             <h1>Voyage</h1>
             {/*<SideBar/>*/}
+            
             <Filter />
             <Result />
         </Container>
