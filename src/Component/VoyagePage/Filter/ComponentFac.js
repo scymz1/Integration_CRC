284e8a72251629e0d2fbf0407da6bc1f866b4b68
ZW <ZEEV2020@outlook.com>
2022-06-13 16:43:06 -0500

    Slider js & css

--- src/Component/VoyagePage/Filter/ComponentFac.js
+++ src/Component/VoyagePage/Filter/ComponentFac.js
@@ -3,12 +3,12 @@ import axios, { Axios } from 'axios';
 import { Component, useContext } from 'react';
 import { render } from '@testing-library/react';
 import Auto from './Autocomplete';
+import Slider from "./Slider"
 import * as React from 'react';
 import {GlobalContext} from "../../App";
 import {
   Typography,
   Container,
-  Slider,
   Button,
   Checkbox,
   FormControlLabel,
@@ -40,7 +40,7 @@ function ComponentFac(props){
   
   switch(varType){
     case "IntegerField" || "DecimalField":
-      return GetSlider(varName, varDisplay);
+      return GetSlider();
     case "BooleanField":
       return <Chip label={modifyName(varDisplay)} color="primary" />;
     case "CharField":
@@ -57,70 +57,72 @@ function modifyName(rawName){
   return res
 }
 
-function GetSlider(varName, varDisplay) {
+function GetSlider() {
+  console.log("get slider request");
+  return <Slider />
 
-  const {options_tree, options_flat, search_object, set_search_object} = useContext(GlobalContext);     // <--------- CONTEXT
+  // const {options_tree, options_flat, search_object, set_search_object} = useContext(GlobalContext);     // <--------- CONTEXT
 
 
-    const [range, setRange] = React.useState([0,0])
-    const [value, setValue] = React.useState([range[0]/2, range[1]/2])
+  //   const [range, setRange] = React.useState([0,0])
+  //   const [value, setValue] = React.useState([range[0]/2, range[1]/2])
 
-    var d = new FormData();
-    d.append('aggregate_fields', varName);
-    //"voyage_slaves_numbers__imp_total_num_slaves_disembarked"
+  //   var d = new FormData();
+  //   d.append('aggregate_fields', varName);
+  //   //"voyage_slaves_numbers__imp_total_num_slaves_disembarked"
 
-    const config =  {
-        method: 'post',
-        baseURL: 'https://voyages3-api.crc.rice.edu/voyage/aggregations',
-        headers: {'Authorization': 'Token 4b6dcc9a91cf8e48fc137eeabe026464064a9446'},
-        data:d
-    }
+  //   const config =  {
+  //       method: 'post',
+  //       baseURL: 'https://voyages3-api.crc.rice.edu/voyage/aggregations',
+  //       headers: {'Authorization': 'Token 4b6dcc9a91cf8e48fc137eeabe026464064a9446'},
+  //       data:d
+  //   }
 
-  React.useEffect(() => {
-    axios(config).then(res => {
-        setRange([Object.values(res.data)[0]['min'], Object.values(res.data)[0]['max']]);
-        setValue([Object.values(res.data)[0]['min'], Object.values(res.data)[0]['max']]);
-    }).then(console.log(config))
-  }, [])
+  // React.useEffect(() => {
+  //   axios(config).then(res => {
+  //       setRange([Object.values(res.data)[0]['min'], Object.values(res.data)[0]['max']]);
+  //       setValue([Object.values(res.data)[0]['min'], Object.values(res.data)[0]['max']]);
+  //   }).then(console.log(config))
+  // }, [])
 
-  function handleCommittedChange(event, newValue) {
-    setValue(newValue); 
-    console.log(varName, ": onchange", value);
-  }
+  // function handleCommittedChange(event, newValue) {
+  //   setValue(newValue); 
+  //   console.log(varName, ": onchange", value);
+  // }
   
-  const handleChange = (event, newValue) => {
-      setValue(newValue); 
-    //   console.log("left " + value[0]);
-    //   console.log("right " + value[1]);
-  };
+  // const handleChange = (event, newValue) => {
+  //     setValue(newValue); 
+  //   //   console.log("left " + value[0]);
+  //   //   console.log("right " + value[1]);
+  // };
 
 
-  return (
-        <>
-        <Typography gutterBottom>{varDisplay}</Typography>
-        <Slider
-            aria-label = {varDisplay}
-            size="small"
-            min = {range[0]}
-            max = {range[1]}
-            // defaultValue={range}
-            getAriaLabel={() => 'Temperature range'}
-            value={value}
-            onChange={handleChange}
-            onChangeCommitted = {handleCommittedChange}
-            valueLabelDisplay="on"
-        />
-        {
+  // return (
+  //       <>
+  //       <Typography gutterBottom>{varDisplay}</Typography>
+  //       <Slider
+  //           aria-label = {varDisplay}
+  //           size="small"
+  //           min = {range[0]}
+  //           max = {range[1]}
+  //           // defaultValue={range}
+  //           getAriaLabel={() => 'Temperature range'}
+  //           value={value}
+  //           onChange={handleChange}
+  //           onChangeCommitted = {handleCommittedChange}
+  //           valueLabelDisplay="on"
+  //       />
+  //       {
 
-          React.useEffect(() => {
-            set_search_object({                     // <---------- UPDATE SEARCH OBJECT
-              ...search_object,
-              varName: [value[0], value[1]]
-            })
-          }, [search_object])
-        }
-        </>
-         );
+  //         React.useEffect(() => {
+  //           set_search_object({                     // <---------- UPDATE SEARCH OBJECT
+  //             ...search_object,
+  //             varName: [value[0], value[1]]
+  //           })
+  //         }, [search_object])
+  //       }
+  //       </>
+  //        );
 }
 
 function GetAuto(){
