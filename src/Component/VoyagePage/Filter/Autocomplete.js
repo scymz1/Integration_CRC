1063cebb80f04d22239605499fb1d8de1e072ebe
Alison Qiu <mythpuzzle@gmail.com>
2022-06-14 12:50:16 -0500

    fixed autocomplete

--- src/Component/VoyagePage/Filter/Autocomplete.js
+++ src/Component/VoyagePage/Filter/Autocomplete.js
@@ -9,11 +9,39 @@ const header={ "Authorization": 'Token bd233c83dceb9a0f70ffd2b47d6cd3a18a095260'
 export default function Auto() {
  const { 
     setTestInput,
-    dropdownOptions,   
+    labels,
+    textInput,   
     value,
     setValue} = React.useContext(AppContext)
 
-    console.log("Before returning autocomplete")
+  const [autocompleteOptions, setautocompleteOptions] = React.useState([]);
+
+    React.useEffect(()=>{
+      console.log('use effect fetch dropdown options')
+      const fetchData = async (labels,textInput) => {
+      //   console.log("Labels.option: ----->", labels.option)
+        var formdata = new FormData();
+        formdata.append(labels.option, textInput);
+
+        //console.log("🚀 ~ label, textInput", label, textInput)
+        var requestOptions = {
+            method: 'POST',
+            headers: header,
+            body: formdata,
+            redirect: 'follow'
+        };
+        fetch("https://voyages3-api.crc.rice.edu/voyage/autocomplete", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            console.log("🚀YAYAYAY fetch is successful!!! result", result)
+            var newOptions = result[labels.option]
+            console.log("🚀 ~ file: Dropdown.js ~ line 43 ~ fetchData ~ newOptions", newOptions)
+            setautocompleteOptions(newOptions) })
+      }
+
+      fetchData(labels[labels.length-1],textInput).catch(console.error)
+    },[])
+
 
   return (
 
@@ -21,7 +49,7 @@ export default function Auto() {
       disablePortal
       autoHighlight
       multiple
-      options={dropdownOptions}
+      options={autocompleteOptions}
       // value={dropdownOptions[0]}
       onChange={(event, newValue) => {
         setValue(oldArray => [newValue][0]);
@@ -29,10 +57,7 @@ export default function Auto() {
       }}
       sx={{ width: 300 }}
       renderInput={(params) => {
-        //params.inputProps.value is what i type in 
-        //console.log("🚀 ~ file: Dropdown.js ~ line 108 ~ Dropdown ~ params", params.InputProps)
         setTestInput(params.inputProps.value)
-        console.log("AUTOCOMPLETE OUTPUT: -----> ", dropdownOptions)
         return <TextField {...params} label="field" />
          
     }}
