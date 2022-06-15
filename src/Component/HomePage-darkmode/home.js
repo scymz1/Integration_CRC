7f9e9845d0815be865894ae7ff0709fb0c36e350
jirany <jiranyang777@gmail.com>
2022-06-15 16:45:48 -0500

    reset to b2a471dff9f804508905a6a8f2123a3f7bc8ce77

---
+++ src/Component/HomePage-darkmode/home.js
@@ -0,0 +1,82 @@
+import * as React from "react";
+import { Component, PureComponent, useState, useEffect } from 'react'
+import TrackVisibility from "react-on-screen";
+import { Animated } from "react-animated-css";
+import { Box, Button, Card, CardContent, CardHeader, List, ListItem,Divider  } from "@mui/material";
+import ResponsiveAppBar from "../NavBar";
+import Container from "@mui/material/Container";
+import BarComponent from "./HomePagePlotly/BarHome";
+import PieComponent from "./HomePagePlotly/PieHome";
+import ScatterComponent from "./HomePagePlotly/ScatterHome";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+// const darkTheme = createTheme({
+//   palette: {
+//     mode: 'dark',
+//   },
+// });
+
+
+export default function Home() {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // for smoothly scrolling
+    });
+  };
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 100) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    });
+  }, []);
+
+  const sample = [<ScatterComponent />,<BarComponent />, <PieComponent />];
+  return (
+    // <ThemeProvider theme={darkTheme}>
+    <div>
+      <ResponsiveAppBar />
+    <Container maxWidth={false}>
+      <List>
+        {sample.map((label) => (
+          <ListItem key={label}>
+            <TrackVisibility offset={300}>
+              {({ isVisible }) => (
+                <Animated
+                  animationIn="slideInLeft"
+                  animationOut="fadeOut"
+                  isVisible={isVisible}
+                  // animationInDuration ="2000"
+                >
+                  {label}
+                  <Divider />
+                </Animated>
+              )}
+            </TrackVisibility>
+          </ListItem>
+        ))}
+   
+      </List>
+      <Box display="flex"
+  justifyContent="flex-end"
+  alignItems="flex-end" padding={4}>
+      {showButton && (
+        <Button variant="outlined" onClick={scrollToTop} startIcon={<ArrowDropUpIcon />}>
+          Up
+        </Button>
+      )}
+      </Box>
+      </Container>
+      
+    </div>
+
+    
+    // </ThemeProvider>
+  );
+}
