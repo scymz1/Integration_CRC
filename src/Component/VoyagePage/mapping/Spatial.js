04286c58fccbdc9325f553ae40aaac182f0505b1
JasonFang99 <jiashengf@smu.edu>
2022-07-06 11:13:47 -0500

    Add live call to geosankey map

--- src/Component/VoyagePage/mapping/Spatial.js
+++ src/Component/VoyagePage/mapping/Spatial.js
@@ -1,35 +1,72 @@
 // The Spatial.js is a component to read a csv file and a Geojson file and draw the geosankey diagram on the map.
 // References: https://github.com/geodesign/spatialsankey, https://github.com/UNFPAmaldives/migration
 
-import { useMapEvents, GeoJSON } from "react-leaflet";
+import React, { useState, useEffect } from 'react'
+import { useMapEvents, GeoJSON, useMap } from "react-leaflet";
 import L from "leaflet";
 import * as d3 from "d3";
-
+import axios from 'axios'
 // import nodes2 from "./voyage_itinerary__imp_principal_region_of_slave_purchase__geo_location__id_voyage_itinerary__imp_broad_region_slave_dis__geo_location__id_Barbados_1800_1810_1_1";
 // import csv2 from "./voyage_itinerary__imp_principal_region_of_slave_purchase__geo_location__id_voyage_itinerary__imp_broad_region_slave_dis__geo_location__id_Barbados_1800_1810_1_1.csv"
 
 import nodes2 from "./voyage_itinerary__imp_port_voyage_begin__geo_location__id_voyage_itinerary__imp_principal_region_of_slave_purchase__geo_location__id_Barbados_Jamaica_1700_1860_0_0";
 import csv2 from "./voyage_itinerary__imp_port_voyage_begin__geo_location__id_voyage_itinerary__imp_principal_region_of_slave_purchase__geo_location__id_Barbados_Jamaica_1700_1860_0_0.csv"
 
+const AUTH_TOKEN = process.env.REACT_APP_AUTHTOKEN;
+axios.defaults.baseURL = process.env.REACT_APP_BASEURL;
+axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+
 var nodeLayers = {};
 var linkLayers = {};
 var selectedNode = null;
 
+var groupby_fields = ['voyage_itinerary__imp_principal_place_of_slave_purchase__geo_location__id', 'voyage_itinerary__imp_principal_port_slave_dis__geo_location__id']
+var value_field_tuple = ['voyage_slaves_numbers__imp_total_num_slaves_disembarked', 'sum']
+var cachename = 'voyage_maps'
+var dataset = [0, 0]
+var output_format = 'geosankey'
 
-  // Drawing nodes and links on the map
+// Drawing nodes and links on the map
   export function ReadFeature() {
+
+    const [csv, setCsv] = useState(null);
+    const [nodes, setNodes] = useState(null);
+
+    // const {search_object} = React.useContext(PastContext);
+    
+    useEffect(() => {
+      var data = new FormData();
+      data.append('groupby_fields', groupby_fields[0]);
+      data.append('groupby_fields', groupby_fields[1]);
+      data.append('value_field_tuple', value_field_tuple[0]);
+      data.append('value_field_tuple', value_field_tuple[1]);
+      data.append('cachename', cachename);
+      data.append('dataset', dataset[0]);
+      data.append('dataset', dataset[1]);
+      data.append('output_format', output_format);
+  
+      axios.post('/voyage/aggroutes', data)
+          .then(function(response) {
+            // console.log(response.data.links)
+            setCsv(response.data.links)
+            setNodes(response.data.nodes)
+          })
+    }, [])
+    
 
     // Function for distinguish if the feature is a waypoint
     const featureWayPt = (feature) => {
         return !feature.properties.name.includes("ocean waypt");
     }
 
+    const map2 = useMap();
+
     const map = useMapEvents( {
       click: (e) => {
         
           var markers = L.markerClusterGroup();
           // Add all features (including waypoints to nodeslayers)
-          L.geoJSON(nodes2.features, {
+          L.geoJSON(nodes.features, {
 
             onEachFeature: function (feature, layer) {
 
@@ -41,7 +78,7 @@ var selectedNode = null;
           });
           
           // Add only actual locations to the map (with clicking events and popups)
-          var nodeLayer = L.geoJSON(nodes2.features, {
+          var nodeLayer = L.geoJSON(nodes.features, {
               filter: featureWayPt,
             
               onEachFeature: function (feature, layer) {
@@ -85,39 +122,39 @@ var selectedNode = null;
               
             });
             map.addLayer(markers)
-        DrawLink(map);
+        DrawLink(map, csv);
       }
     });
-
     return null;
   }
 
   // Function to draw the links
-  function DrawLink(map) {
+  function DrawLink(map, links) {
   
-    var links = [];
+    // var links = [];
 
-    d3.csv(csv2, function(data){
-      links = [...links, data]
-    }).then(function() {
+    // console.log("Links: ", csv);
 
+    // d3.csv(csv, function(data){
+    //   links = [...links, data]
+    // }).then(function() {
 
-      var valueMin = d3.min(links, function(l) { console.log("Flow: ", l.flow); return (l.source != l.target) ? parseInt(l.flow) : null; });
-      var valueMax = d3.max(links, function(l) { return (l.source != l.target) ? parseInt(l.flow) : null; });
+      var valueMin = d3.min(links, function(l) { console.log("Flow: ", l[2]); return (l[0] != l[1]) ? parseInt(l[2]) : null; });
+      var valueMax = d3.max(links, function(l) { return (l[0] != l[1]) ? parseInt(l[2]) : null; });
 
       var valueScale = d3.scaleLinear()
                             .domain([valueMin, valueMax])
-                            .range([1, 20]);
+                            .range([1, 10]);
 
       links.forEach(function (link) {
-        if (link.source != link.target) {
-          var path = [link.source, link.target].join('-');
-          var pathReverse = [link.target, link.source].join('-');
+        if (link[0] != link[1]) {
+          var path = [link[0], link[1]].join('-');
+          var pathReverse = [link[1], link[0]].join('-');
 
-          var lineWeight = valueScale(link.flow);
+          var lineWeight = valueScale(link[2]);
 
           
-          var lineCenterLatLng = L.polyline([ nodeLayers[link.source].layer._latlng, nodeLayers[link.target].layer._latlng ])
+          var lineCenterLatLng = L.polyline([ nodeLayers[link[0]].layer._latlng, nodeLayers[link[1]].layer._latlng ])
                     .getBounds()
                     .getCenter();
 
@@ -139,15 +176,15 @@ var selectedNode = null;
 
           var line = L.polyline(
             [ 
-                nodeLayers[link.source].layer._latlng, 
+                nodeLayers[link[0]].layer._latlng, 
                 lineBreakLatLng, 
-                nodeLayers[link.target].layer._latlng
+                nodeLayers[link[1]].layer._latlng
             ], {
               weight: lineWeight
             });
 
             var feature = L.featureGroup([line])
-                .bindPopup('<p3>' + link.source + '</p3> to <p3>' + link.target + '</p3>' + '<br>' + '<p4>' + link.flow + ' migrants' + '</p4>')
+                .bindPopup('<p3>' + link[0] + '</p3> to <p3>' + link[1] + '</p3>' + '<br>' + '<p4>' + link[2] + ' migrants' + '</p4>')
                 .on('click', function(e) {
                     
                     this.openPopup();
@@ -165,7 +202,8 @@ var selectedNode = null;
           }
         }
       });
-    })
+    // }
+    // )
     // });
     return null;
   }
