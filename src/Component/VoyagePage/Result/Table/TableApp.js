8942be6fdf8628be7aa3f7da9f109ac7728efff2
Lize Chen <chenlize96@gmail.com>
2022-07-12 15:48:05 -0500

    Separated Modal from Table

--- src/Component/VoyagePage/Result/Table/TableApp.js
+++ src/Component/VoyagePage/Result/Table/TableApp.js
@@ -1,23 +1,39 @@
-import * as React from "react";
 import ColSelector from "./ColSelector";
 import Table from "./Table";
-//import {VoyageContext} from "../../VoyageApp"
-//import Button from "@mui/material/Button";
+import Modal from "./TableModal";
+import React, { useState } from "react";
 export const ColContext = React.createContext({});
 
-export default function (props) {
+function TableApp(props) {
   const [cols, setCols] = React.useState(["id"]);
   const { endpoint } = React.useContext(props.context);
+  const [open, setOpen] = useState(false);
+  const [id, setId] = useState(0);
+  const [info, setInfo] = useState([]);
 
   return (
     <div>
       {/* <Button onClick={()=>console.log("options_tree:", endpoint)}>print options_tree</Button> */}
       <ColContext.Provider
-        value={{ cols, setCols, endpoint, checkbox: false, modal: true }}
+        value={{
+          cols,
+          setCols,
+          endpoint,
+          checkbox: false,
+          modal: true,
+          id,
+          setId,
+          open,
+          setOpen,
+          info,
+          setInfo,
+        }}
       >
         <ColSelector context={ColContext} />
         <Table context={ColContext} />
+        <Modal context={ColContext} />
       </ColContext.Provider>
     </div>
   );
 }
+export default TableApp;
