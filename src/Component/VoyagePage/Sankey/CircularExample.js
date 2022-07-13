3032e3acaf0410f08112009f85b8f42c49b90df0
Zhihao <wanz@rice.edu>
2022-07-13 16:18:25 -0500

    dropdown Sankey

--- src/Component/VoyagePage/Sankey/CircularExample.js
+++ src/Component/VoyagePage/Sankey/CircularExample.js
@@ -10,6 +10,12 @@ import Sankey from "./CircularSankey";
 import { useState } from "react";
 import {Button} from "@mui/material";
 import { forEach, isError } from "lodash";
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import {voyage_pivot_tables, voyage_maps} from "./vars"
+import FormControl from '@mui/material/FormControl';
+import { FormControlLabel, RadioGroup } from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
 // import { type } from "@testing-library/user-event/dist/type";
 // import myJson from './sample.json';
 // console.log("🐢this is the myJson" + myJson);
@@ -43,15 +49,17 @@ import { forEach, isError } from "lodash";
 //   console.log(error);
 // });
 
+
 export default function SankeyExample(props) {
-  
-  const {isLoading, error, data} = useQuery('',() => {
+
+
+  const {isLoading, error, data, refetch} = useQuery('',() => {
     var myHeaders = new Headers();
 myHeaders.append("Authorization", "Token d4acb77be3a259c23ee006c70a20d70f7c42ec23");
 
 var formdata = new FormData();
-formdata.append("groupby_fields", "voyage_itinerary__imp_broad_region_slave_dis__geo_location__name");
-formdata.append("groupby_fields", "voyage_itinerary__imp_principal_region_of_slave_purchase__geo_location__name");
+formdata.append("groupby_fields", option.fieldSource);
+formdata.append("groupby_fields", option.fieldTarget);
 formdata.append("value_field_tuple", "voyage_slaves_numbers__imp_total_num_slaves_embarked");
 formdata.append("value_field_tuple", "sum");
 formdata.append("cachename", "voyage_pivot_tables");
@@ -66,6 +74,9 @@ var requestOptions = {
 return fetch("https://voyages3-api.crc.rice.edu/voyage/crosstabs", requestOptions)
   .then(response => response.json())
   .then(result => {
+
+    // console.log(result)
+    // return result
     let allNodes= new Set()
     let nodes = []
     let links = []
@@ -122,6 +133,7 @@ return fetch("https://voyages3-api.crc.rice.edu/voyage/crosstabs", requestOption
       nodes: nodes,
       links:links
     }
+// return
 
   })
   .catch(error => console.log('error', error));
@@ -136,6 +148,30 @@ return fetch("https://voyages3-api.crc.rice.edu/voyage/crosstabs", requestOption
     nodePadding: 10,
     component: "Sankey",
   });
+
+  const [optionSet, setOptionSet] = useState([...voyage_pivot_tables])
+  // const [optionSet2, setOptionSet2] = useState([...voyage_pivot_tables])
+  const [option, setOption] = useState({
+    fieldSource: voyage_pivot_tables[10],
+    fieldTarget: voyage_pivot_tables[8]
+})
+
+const handleChange = (event, name, type) => {
+  console.log(name, event.target.value)
+  setOption({
+      ...option,
+      [name]: event.target.value,
+
+  })
+
+  // if(type === 1){
+  //   setOptionSet2(optionSet2.filter((item)=>item !== event.target.value))
+  // }else if(type === 2){
+  //   setOptionSet(optionSet.filter((item)=>item !== event.target.value))
+  // }
+  
+  refetch()
+}
 
   const path = linkHorizontal()
     .source((d) => [d.source.x1, d.y0])
@@ -161,6 +197,44 @@ return fetch("https://voyages3-api.crc.rice.edu/voyage/crosstabs", requestOption
   return (
     <div>
       <Button onClick={()=>console.log("data:", data)}>print data</Button>
+      <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Source Field</InputLabel>
+      <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={option.fieldSource}
+                            label="Source Field"
+                            onChange={(event) => {handleChange(event, "fieldSource")}}
+                            name="source"
+                        >
+                            {optionSet.map((option) => (
+                                <MenuItem key={option} value={option}>
+                                    {option}
+                                </MenuItem>
+                            ))}
+
+      </Select>
+      </FormControl>
+
+      <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Target Field</InputLabel>
+      <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={option.fieldTarget}
+                            label="Target Field"
+                            onChange={(event) => {handleChange(event, "fieldTarget")}}
+                            name="target"
+                        >
+                            {optionSet.map((option) => (
+                                <MenuItem key={option} value={option}>
+                                    {option}
+                                </MenuItem>
+                            ))}
+
+      </Select>
+      </FormControl>
+
       <svg
         width={width + margin.left + margin.right}
         height={height + margin.top + margin.bottom}
