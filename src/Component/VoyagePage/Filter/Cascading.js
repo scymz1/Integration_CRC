d8307c4b64f0a513f2f59ec25d8f3f368f21997c
JiachengSun0520 <js211@rice.edu>
2022-07-21 10:07:15 -0500

    labels,vars updated

--- src/Component/VoyagePage/Filter/Cascading.js
+++ src/Component/VoyagePage/Filter/Cascading.js
@@ -97,6 +97,7 @@ function Cascading(props) {
                         // : null
                         : containsOnly(nodes[key])
                             ?  renderTree(nodes[key], key) 
+                            // ? null
                             : <NestedMenuItem
                                 key={key}
                                 // label={options_flat[nameConcat(name,key)].label}
