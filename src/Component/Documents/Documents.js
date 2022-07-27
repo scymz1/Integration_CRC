4edc98c4064d70bf919b0c0b70be8d409c67f31d
Fang <60528191+haoyuf1@users.noreply.github.com>
2022-07-27 11:47:51 -0500

    Rm UV Modal from Doc and Modify struct

--- src/Component/Documents/Documents.js
+++ src/Component/Documents/Documents.js
@@ -1,60 +1,19 @@
-import React, { useRef, useState, useEffect, useLayoutEffect } from "react";
-import { Button, Modal, Box, Container, Avatar } from "@mui/material";
+import React, { useEffect, useState, useContext } from "react";
+import { Button, Modal, Box, Card, CardContent, Typography, Container, Avatar } from "@mui/material";
+
 import ResponsiveAppBar from "../NavBar";
+import { DocContext } from "./DocumentsApp";
+import UVModal from "./UVModal"
 
-import "universalviewer/dist/esm/index.css";
-import { init } from "universalviewer";
-
-function useUniversalViewer(ref, options) {
-    const [uv, setUv] = useState();
-  
-    useLayoutEffect(() => {
-      const currentUv = init(ref.current, options);
-      setUv(currentUv);
-  
-      return () => {
-        currentUv.dispose();
-      };
-    }, []);
-  
-    return uv;
-  }
-
-const UV = ({ manifest, parentWidth }) => {
-  const el = useRef();
-  const viewer = useUniversalViewer(
-    el,
-    {
-      manifest,
-    }
-  );
-  return <div ref={el} 
-              className="uv" 
-              style={{
-                        width: "50vw",
-                        height: "50vh"
-                    }}/>;
-};
-
-export default function Documents() {
-    const DocContext = React.createContext({});
+export default function Documents(props) {
+    const { dataSet, pageType } = useContext(DocContext);
+    const UVContext = React.createContext({});
     const [manifest, setManifest]= useState("http://150.136.1.167/iiif/3/766");
     const [title, setTitle] = useState(null);
     const [image, setImage] = useState(null);
-    const [open, setOpen] = useState(false);
-
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-
-    const modalStyle = {
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        p: 4,
-        overflow: "scroll",
-        maxHeight: 500,
-      };
+    const [uvOpen, setUVOpen] = useState(false);
+        const handleUVOpen = () => setUVOpen(true);
+        const handleUVClose = () => setUVOpen(false);
 
     useEffect(() => {
         fetch(manifest)
@@ -63,28 +22,22 @@ export default function Documents() {
                     setTitle(response.label.none);
                     setImage(response.thumbnail[0].id)
                 })
-    },[]);
+    },[manifest]);
     
     return (
         <div>
-            <DocContext.Provider value={{dataSet:"0", pageType:"home"}}>
-                <ResponsiveAppBar context={DocContext}/>
-            </DocContext.Provider>
-            <Button onClick={handleOpen} startIcon={<Avatar src={image}/>} >
+            <ResponsiveAppBar context={DocContext}/>
+            <Button onClick={handleUVOpen} startIcon={<Avatar src={image}/>} >
                 {title}
             </Button>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box sx={modalStyle}>
-                {/* <div> */}
-                    <UV manifest={manifest} />
-                {/* </div> */}
-                </Box>
-            </Modal>
+            <UVContext.Provider
+                value={{
+                  manifest, setManifest, 
+                  uvOpen, setUVOpen, 
+                  handleUVOpen, handleUVClose
+                }}>
+                <UVModal useContext={ UVContext }/>
+            </UVContext.Provider>
         </div>
     );
 }
