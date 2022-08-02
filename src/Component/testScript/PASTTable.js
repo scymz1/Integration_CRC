2875a32c16acdf08aa72d692ea3ef76d2eb482ed
Lize Chen <chenlize96@gmail.com>
2022-08-02 15:58:49 -0500

    Server-side Sorting

--- src/Component/testScript/PASTTable.js
+++ src/Component/testScript/PASTTable.js
@@ -1,21 +1,35 @@
-import {DataGrid, GridToolbar} from "@mui/x-data-grid";
-import {LinearProgress} from "@mui/material";
-import {useMemo, useState} from "react";
-import {enslaved_default_list} from "../PAST/vars";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { LinearProgress } from "@mui/material";
+import { useMemo, useState } from "react";
+import { enslaved_default_list } from "../PAST/vars";
 import * as options_flat from "../util/enslaved_options.json";
 import Cell from "./Cell";
 
 export default function PASTTable(props) {
-  const {dataList,totalRows, pagination, setPagination, isLoading, set_search_object} = props.state
-  const defaultColumns = useMemo(()=>{
-    const result = []
-    enslaved_default_list.forEach((column)=>{
-      result.push({field: column, headerName: options_flat[column].flatlabel, renderCell: Cell,
-        flex:options_flat[column].flatlabel.length*6, minWidth: options_flat[column].flatlabel.length*6+100})
-    })
+  const {
+    dataList,
+    totalRows,
+    pagination,
+    setPagination,
+    sortModel,
+    setSortModel,
+    isLoading,
+    set_search_object,
+  } = props.state;
+  const defaultColumns = useMemo(() => {
+    const result = [];
+    enslaved_default_list.forEach((column) => {
+      result.push({
+        field: column,
+        headerName: options_flat[column].flatlabel,
+        renderCell: Cell,
+        flex: options_flat[column].flatlabel.length * 6,
+        minWidth: options_flat[column].flatlabel.length * 6 + 100,
+      });
+    });
     return result;
-  }, [enslaved_default_list])
-  const [columns, setColumns] = useState(defaultColumns)
+  }, [enslaved_default_list]);
+  const [columns, setColumns] = useState(defaultColumns);
 
   return (
     <DataGrid
@@ -30,19 +44,19 @@ export default function PASTTable(props) {
       // componentsProps={{}}
       pagination
       paginationMode="server"
-      rowsPerPageOptions={[10, 20, 50]}
+      rowsPerPageOptions={[10, 15, 20, 25]}
       page={pagination.currPage}
       pageSize={pagination.rowsPerPage}
       onPageChange={(newPage) => {
-        setPagination({...pagination, currPage: newPage})
+        setPagination({ ...pagination, currPage: newPage });
       }}
       onPageSizeChange={(newPageSize) => {
-        setPagination({...pagination, rowsPerPage: newPageSize})
+        setPagination({ ...pagination, rowsPerPage: newPageSize });
       }}
-
-      // sortingMode="server"
-      // onSortModelChange={(sortModel)=>{
-      //   console.log(sortModel)}}
+      sortingMode="server" //sortModel={sortModel}
+      onSortModelChange={(newSortModel) => {
+        setSortModel(newSortModel);
+      }}
     />
-  )
+  );
 }
