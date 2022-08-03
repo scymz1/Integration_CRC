a9089f3a8722a39ba3f407468af01306d4094fd4
scymz1 <mz50@rice.edu>
2022-08-03 11:35:52 -0500

    resize column width in table

--- src/Component/testScript/PASTTable.js
+++ src/Component/testScript/PASTTable.js
@@ -7,7 +7,7 @@ import {
   useGridSelector,
 } from "@mui/x-data-grid";
 import { LinearProgress } from "@mui/material";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { enslaved_default_list } from "../PAST/vars";
 import * as options_flat from "../util/enslaved_options.json";
 import Cell from "./Cell";
@@ -19,6 +19,7 @@ import MenuItem from "@mui/material/MenuItem";
 import Stack from "@mui/material/Stack";
 import Paper from "@mui/material/Paper";
 import { styled } from "@mui/material/styles";
+import { max } from "lodash";
 
 export default function PASTTable(props) {
   const {
@@ -31,6 +32,34 @@ export default function PASTTable(props) {
     isLoading,
     set_search_object,
   } = props.state;
+  const lengths = useMemo(()=>{
+    var temp={};
+    dataList.forEach((row)=>{
+      for (const [key, value] of Object.entries(row)) {
+        switch(key){
+          case "transactions__transaction__enslavers__enslaver_alias__identity__principal_alias":
+            var curlength=value?value.length*200:200;
+            temp[key]=temp[key]?Math.max(temp[key], curlength):curlength;
+            break;
+          default:
+            var curlength=250
+            //console.log("abdc", key, typeof(value))
+            if(typeof(value)==="number"){
+              curlength=value.toString().length*20;
+            }
+            else if(typeof(value)==="string"){
+              curlength=value.length*10;
+            }
+            // console.log("abdc", key, curlength, typeof(temp[key])!=="undefined", temp[key])
+            temp[key]=temp[key]? Math.max(temp[key], curlength):curlength;
+            break;
+        };
+      }
+    })
+    enslaved_default_list.forEach((column) => {console.log(column, temp[column])})
+    //console.log("temp", temp);
+    return temp;
+  }, [dataList]);
   const defaultColumns = useMemo(() => {
     const result = [];
     enslaved_default_list.forEach((column) => {
@@ -39,13 +68,15 @@ export default function PASTTable(props) {
         headerName: options_flat[column].flatlabel,
         renderCell: Cell,
         //minWidth: 160,
-        //flex: 1,
-        minWidth: options_flat[column].flatlabel.length * 6 + 100,
-        maxWidth: 300,
+        flex: lengths[column],
+        minWidth: Math.max(options_flat[column].flatlabel.length*8.8, 100),
+        maxWidth: 1000,
       });
+      
     });
+    
     return result;
-  }, [enslaved_default_list]);
+  }, [enslaved_default_list, lengths]);
   const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
     ...theme.typography.body2,
@@ -90,13 +121,12 @@ export default function PASTTable(props) {
       </Stack>
     );
   }
-  const [columns, setColumns] = useState(defaultColumns);
-
+  //const [columns, setColumns] = useState(defaultColumns);
   return (
     <div style={{ width: "100%" }}>
       <DataGrid
         autoHeight={true}
-        columns={columns}
+        columns={defaultColumns}
         rows={dataList}
         rowCount={totalRows}
         loading={isLoading}
