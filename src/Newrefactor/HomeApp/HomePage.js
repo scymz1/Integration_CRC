0987f2dbd3c5938f93d528a51153896d0e62a4d3
JiachengSun0520 <js211@rice.edu>
2022-08-04 15:07:34 -0500

    home page lazy loading

--- src/Newrefactor/HomeApp/HomePage.js
+++ src/Newrefactor/HomeApp/HomePage.js
@@ -1,14 +1,12 @@
 import * as React from "react";
-import {useEffect, useState} from "react";
-import TrackVisibility from "react-on-screen";
-import {Animated} from "react-animated-css";
-import {Box, Button, Divider, List, ListItem} from "@mui/material";
+import { Box, Button, List} from "@mui/material";
 import Navbar from "../CommonComponent/NavBar";
 import Container from "@mui/material/Container";
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import BarComponent from "./Component/BarSample";
 import PieComponent from "./Component/PieSample";
 import ScatterComponent from "./Component/ScatterSample";
+
 // import TableHome from "./HomePagePlotly/TableHome/TableHome";
 // import SankeyHome from "./HomePagePlotly/SankeyHome";
 // import StoryHome from "./HomePagePlotly/StoryHome";
@@ -23,10 +21,10 @@ import ScatterComponent from "./Component/ScatterSample";
 import {
   useWindowSize,
 } from '@react-hook/window-size'
+import ArchiveHome from "./ArchiveHome";
 
 
 export default function Home() {
-//   const HomeContext = React.createContext({});
   const [width, height] = useWindowSize()
 
   const scrollToTop = () => {
@@ -35,58 +33,26 @@ export default function Home() {
       behavior: 'smooth' // for smoothly scrolling
     });
   };
-  const [showButton, setShowButton] = useState(false);
 
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.pageYOffset > 100) {
-        setShowButton(true);
-      } else {
-        setShowButton(false);
-      }
-    });
-  }, []);
   const dataset = "0";
-  const state = {dataset, pageType: "home"};
-  const sample = [<ScatterComponent/>, <BarComponent/>, <PieComponent/>];
+  const state = { dataset, pageType: "home" };
+  const sample = [<BarComponent />, <PieComponent />, <PieComponent />, <PieComponent />, <PieComponent />];
   return (
-    // <ThemeProvider theme={darkTheme}>
-      <div>
-        <Navbar state={state}/>
-        <Container maxWidth={false}>
-          {/* <NetworkHome/> */}
-          <List>
-            {sample.map((label, index) => (
-              <ListItem key={index}>
-                <TrackVisibility offset={width<800?1200:200}>
-                  {({isVisible}) => (
-                    <Animated
-                      animationIn="slideInLeft"
-                      animationOut="fadeOut"
-                      isVisible={isVisible}
-                      animationInDelay= {400}
-                      animationOutDelay= {500}
-                      animationOutDuration = {3000}
-                    >
-                      {label}
-                      <Divider/>
-                    </Animated>
-                  )}
-                </TrackVisibility>
-              </ListItem>
-            ))}
-
-          </List>
-          <Box display="flex"
-               justifyContent="flex-end"
-               alignItems="flex-end" padding={4}>
-            {showButton && (
-              <Button variant="outlined" onClick={scrollToTop} startIcon={<ArrowDropUpIcon/>}>
-                Up
-              </Button>
-            )}
-          </Box>
-        </Container>
-      </div>
+    <div>
+      <Navbar state={state} />
+      <Container maxWidth={false} >
+        <List>
+              <ScatterComponent /> 
+              <ArchiveHome components={sample} scrollPosition={window.scrollY}/>
+        </List>
+        <Box display="flex"
+          justifyContent="flex-end"
+          alignItems="flex-end" padding={4}>
+            <Button variant="outlined" onClick={scrollToTop} startIcon={<ArrowDropUpIcon />}>
+              Up
+            </Button>
+        </Box>
+      </Container>
+    </div>
   );
 }
