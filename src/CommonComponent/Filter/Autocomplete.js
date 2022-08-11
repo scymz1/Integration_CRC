69558731a957e262fd8127b5e126d1c78a837f40
JasonFang99 <jiashengf@smu.edu>
2022-08-11 13:20:41 -0500

    Change slider endpoint selection

--- src/CommonComponent/Filter/Autocomplete.js
+++ src/CommonComponent/Filter/Autocomplete.js
@@ -14,6 +14,9 @@ export default function Auto(props) {
     const [autocompleteOptions, setautocompleteOptions] = React.useState([]);
 
     const endpoint = (() => {
+
+        console.log("Page Type: ", pageType)
+
         switch (pageType) {
             case "enslaved  ":
                 return "past/enslaved/"
@@ -23,6 +26,7 @@ export default function Auto(props) {
                 return "voyage/"
         }
     })()
+    
 
     console.log("Page Type: ", pageType)
 
